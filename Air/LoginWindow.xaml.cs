bbe3d8bf12a81127af321904936595df79be6a7a
mrjakebox <visualstudio.belstu@outlook.com>
2017-12-04 14:18:51 +0300

    add login window

---
+++ Air/LoginWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Air
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MinimizeClick(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void CloseClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void DradWindow(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
