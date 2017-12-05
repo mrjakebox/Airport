af488d0651d5376a0bc0fb1972f3e165c04fc9f5
mrjakebox <visualstudio.belstu@outlook.com>
2017-12-05 23:26:55 +0300

    create CountryRepository

---
+++ Air/ModelRepository/CountryRepository.cs
@@ -0,0 +1,113 @@
+﻿using Air.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Air.ModelRepository
+{
+    public class CountryRepository : IRepository<CountryModel>
+    {
+        private static SqlConnection _connection;
+
+        private readonly SqlTransaction _transaction;
+
+        public CountryRepository(SqlTransaction transaction)
+        {
+            _connection = ModelConnection.SqlConnection.Instance.DbConnection as SqlConnection;
+            _transaction = transaction;
+        }
+
+        public CountryModel CreateModel(SqlDataReader reader)
+        {
+            return new CountryModel
+            {
+                CountryID = Convert.ToInt32(reader["CountryID"]),
+                CountryName = reader["CountryName"].ToString()
+            };
+        }
+
+        public bool Create(CountryModel item)
+        {
+            SqlCommand command = _connection.CreateCommand();
+            command.CommandType = CommandType.StoredProcedure;
+            command.CommandText = "CountryCreate";
+            command.Transaction = _transaction;
+            command.Parameters.AddRange(new[]
+            {
+                new SqlParameter("@CountryID", item.CountryID),
+                new SqlParameter("@CountryName", item.CountryName)
+            });
+            return command.ExecuteNonQuery() == 1;
+        }
+
+        public bool Delete(CountryModel item)
+        {
+            SqlCommand command = _connection.CreateCommand();
+            command.CommandType = CommandType.StoredProcedure;
+            command.CommandText = "CountryDelete";
+            command.Transaction = _transaction;
+            command.Parameters.Add(new SqlParameter("@CountryID", item.CountryID));
+
+            return command.ExecuteNonQuery() == 1;
+        }
+
+        public void Dispose()
+        {
+            _connection.Dispose();
+        }
+
+        public CountryModel Select(CountryModel item)
+        {
+            SqlCommand command = _connection.CreateCommand();
+            command.CommandType = CommandType.StoredProcedure;
+            command.CommandText = "CountrySelect";
+            command.Transaction = _transaction;
+            command.Parameters.Add(new SqlParameter("@CountryID", item.CountryID));
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            return CreateModel(reader);
+        }
+
+        public IEnumerable<CountryModel> SelectList()
+        {
+            List<CountryModel> countries = new List<CountryModel>();
+
+            SqlCommand command = _connection.CreateCommand();
+            command.CommandType = CommandType.StoredProcedure;
+            command.CommandText = "CountrySelectList";
+            command.Transaction = _transaction;
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    countries.Add(CreateModel(reader));
+                }
+            }
+            reader.Close();
+
+            return countries;
+        }
+
+        public bool Update(CountryModel item)
+        {
+            SqlCommand command = _connection.CreateCommand();
+            command.CommandType = CommandType.StoredProcedure;
+            command.CommandText = "CountryUpdate";
+            command.Transaction = _transaction;
+            command.Parameters.AddRange(new[]
+            {
+                new SqlParameter("@CountryID", item.CountryID),
+                new SqlParameter("@CountryName", item.CountryName)
+            });
+            return command.ExecuteNonQuery() == 1;
+        }
+    }
+}
